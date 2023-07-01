d68bf71c09377d862a96088c52e22319f1e8bd45
nduka-junior <ndukajunior4@gmail.com>
2023-07-01 11:41:33 +0100

    vercel upload

--- src/lib/getLaunch/getLaunches.js
+++ src/lib/getLaunch/getLaunches.js
@@ -1,5 +1,7 @@
 export default async function getlaunces() {
-  const getlaunches = await fetch("https://api.spacexdata.com/v5/launches");
+  const getlaunches = await fetch("https://api.spacexdata.com/v5/launches", {
+    next: { revalidate: 172800 },
+  });
   const res = await getlaunches.json();
   if (!res) throw new Error("Unable to fetch data");
   return res;
--- src/lib/getRocket/getRockets.js
+++ src/lib/getRocket/getRockets.js
@@ -1,5 +1,7 @@
 export default async function getRockets() {
-    const getRockets = await fetch("https://api.spacexdata.com/v4/rockets");
+    const getRockets = await fetch("https://api.spacexdata.com/v4/rockets", {
+      next: { revalidate: 172800 },
+    });
     const res = await getRockets.json()
     if (!res) throw new Error("Couldn't Fetch Data")
     return res
