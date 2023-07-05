535563177f04cc4f7809f63123e1fa627f6a6ec6
nduka-junior <ndukajunior4@gmail.com>
2023-07-05 23:20:22 +0100

    prod upload

--- src/app/starlink/page.js
+++ src/app/starlink/page.js
@@ -3,13 +3,17 @@
 import { useState, useEffect } from "react";
 import dynamic from "next/dynamic";
 function page() {
+   const [mounted, setMounted] = useState(false);
   const MapWithNoSSR = dynamic(() => import("../../components/Map"), {
     ssr: false,
   });
+   useEffect(() => {
+     setMounted(true);
+   }, []);
 
   return (
     <div id="map">
-      <MapWithNoSSR />
+    { mounted && <MapWithNoSSR />}
     </div>
   );
 }
