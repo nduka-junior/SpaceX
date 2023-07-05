163d625b5017f556c09a23ee70e237f0bb51a781
nduka-junior <ndukajunior4@gmail.com>
2023-07-05 23:57:40 +0100

    prod upload

--- src/app/starlink/page.js
+++ src/app/starlink/page.js
@@ -1,21 +1,16 @@
 "use client";
-
 import { useState, useEffect } from "react";
 import dynamic from "next/dynamic";
-function page() {
-   const [mounted, setMounted] = useState(false);
+function Page(){
+  const [mounted, setMounted] = useState(false);
   const MapWithNoSSR = dynamic(() => import("../../components/Map"), {
     ssr: false,
   });
-   useEffect(() => {
-     setMounted(true);
-   }, []);
+  useEffect(() => {
+    setMounted(true);
+  }, []);
 
-  return (
-    <div id="map">
-    { mounted && <MapWithNoSSR />}
-    </div>
-  );
+  return <div id="map">{mounted && <MapWithNoSSR />}</div>;
 }
 
-export default page;
+export default Page;
