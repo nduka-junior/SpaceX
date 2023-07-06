2f753cc63390b7028f4b3fcadce6c12eeaf53aa5
nduka-junior <ndukajunior4@gmail.com>
2023-07-06 22:19:48 +0100

    added loading and footer

--- src/components/Map.js
+++ src/components/Map.js
@@ -6,15 +6,21 @@ import {
   Popup,
   useMapEvents,
 } from "react-leaflet";
-import { useEffect, useState, } from "react";
+import { useState } from "react";
 import data from "@/lib/dd.json";
 import React from "react";
 import formatDate from "@/lib/date";
 import Link from "next/link";
-import { faHourglass1 } from "@fortawesome/free-solid-svg-icons";
+import L from "leaflet";
 async function Map() {
-//   const starlinks = await getStarlink();
-    const starlinks = data;
+  const starlinks = await getStarlink();
+  const myIcon = new L.Icon({
+      iconUrl: "https://creazilla-store.fra1.digitaloceanspaces.com/icons/3202178/satellite-icon-md.png",
+      iconRetinaUrl:"https://creazilla-store.fra1.digitaloceanspaces.com/icons/3202178/satellite-icon-md.png",
+    popupAnchor: [-0, -0],
+    iconSize: [32, 32],
+  });
+  // const starlinks = data;
   const filterStarlinks = starlinks.filter((starlink) => {
     return starlink.longitude !== null && starlink.latitude !== null;
   });
@@ -31,75 +37,72 @@ async function Map() {
     });
 
     return position === null ? null : (
-      <Marker position={position}>
+      <Marker position={position} icon={myIcon}>
         <Popup>You are here</Popup>
       </Marker>
     );
   }
 
-    return (
-   
-        <MapContainer
-            preferCanvas={true}
-          center={{ lat: 51.505, lng: -0.09 }}
-          zoom={10}
-          scrollWheelZoom={false}
-          style={{ height: "85vh", width: "100vw" }}
-        >
-          <TileLayer
-            url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
-            attribution='Map data &copy; <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, <a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery &copy; <a href="https://www.mapbox.com/">Mapbox</a>'
-          />
+  return (
+    <MapContainer
+      preferCanvas={true}
+      center={{ lat: 51.505, lng: -0.09 }}
+      zoom={10}
+      scrollWheelZoom={false}
+      style={{ height: "85vh", width: "100vw" }}
+    >
+      <TileLayer
+        url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+        attribution='Map data &copy; <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, <a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery &copy; <a href="https://www.mapbox.com/">Mapbox</a>'
+      />
 
-          <LocationMarker />
+      <LocationMarker />
 
-          {filterStarlinks.map((starlink, index) => {
-            return (
-              <div key={index}>
-                <Marker
-                  position={[starlink.latitude, starlink.longitude]}
-                  animate={true}
-                >
-                  <Popup>
-                    <div>
-                      <h6 className="text-[14px]">
-                        Name: {starlink.spaceTrack?.OBJECT_NAME}
-                      </h6>
-                      <h6 className="text-[12px]">
-                        <span className="text-[16px] mr-1"> 🚀</span>
-                        {formatDate(starlink.spaceTrack?.LAUNCH_DATE)}
-                      </h6>
-                      {starlink.version && (
-                        <h6 className="text-[14px]">
-                          Version: {starlink.version}
-                        </h6>
-                      )}
-                      {starlink.height_km && (
-                        <h6 className="text-[14px]">
-                          Height: {Math.trunc(starlink.height_km)}km
-                        </h6>
-                      )}
+      {filterStarlinks.slice(0, 400).map((starlink, index) => {
+        return (
+          <div key={index}>
+            <Marker
+              position={[starlink.latitude, starlink.longitude]}
+              animate={true}
+              icon={myIcon}
+            >
+              <Popup>
+                <div>
+                  <h6 className="text-[14px]">
+                    Name: {starlink.spaceTrack?.OBJECT_NAME}
+                  </h6>
+                  <h6 className="text-[12px]">
+                    <span className="text-[16px] mr-1"> 🚀</span>
+                    {formatDate(starlink.spaceTrack?.LAUNCH_DATE)}
+                  </h6>
+                  {starlink.version && (
+                    <h6 className="text-[14px]">Version: {starlink.version}</h6>
+                  )}
+                  {starlink.height_km && (
+                    <h6 className="text-[14px]">
+                      Height: {Math.trunc(starlink.height_km)}km
+                    </h6>
+                  )}
 
-                      {starlink.launch && (
-                        <h6 className="text-[14px]">
-                          Launch:
-                          <Link
-                            className=" text-blue"
-                            href={`./launches/${starlink.launch}`}
-                          >
-                            Rocket Used
-                          </Link>
-                        </h6>
-                      )}
-                    </div>
-                  </Popup>
-                </Marker>
-              </div>
-            );
-          })}
-        </MapContainer>
-  
-    );
+                  {starlink.launch && (
+                    <h6 className="text-[14px]">
+                      Launch:
+                      <Link
+                        className=" text-blue"
+                        href={`./launches/${starlink.launch}`}
+                      >
+                        Rocket Used
+                      </Link>
+                    </h6>
+                  )}
+                </div>
+              </Popup>
+            </Marker>
+          </div>
+        );
+      })}
+    </MapContainer>
+  );
 }
 
 export default Map;
